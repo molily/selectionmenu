193af5d001856b69cd9fed7d2ba85ea8499bc946
Mathias Schaefer (molily) <zapperlott@gmail.com>
2018-05-27 23:02:35 +0200

    New e-mail address

--- copylink.js
+++ copylink.js
@@ -1,7 +1,7 @@
 /*
 CopyLink 1.1
 http://github.com/molily/selectionmenu
-by molily (zapperlott@gmail.com, http://molily.de/)
+by molily (molily@mailbox.org, http://molily.de/)
 
 EN: CopyLink automatically inserts a source reference when the user copies text from a page
 DE: CopyLink fügt automatisch eine Quellenreferenz ein, wenn der Benutzer Text von einer Seite kopiert
@@ -18,14 +18,14 @@ DE: Kopieren, Verteilen und Aendern ohne Einschraenkungen erlaubt
 // DE: Erzeuge einen privaten Scope durch eine anonyme Funktion,
 // DE: speichere den Rückgabwert in einer globalen Variable
 var CopyLink = (function (window, document) {
-	
+
 	// EN: The element which is inserted when copying
 	// DE: Das Element, welche beim Kopieren eingefügt wird
 	var span = null;
-	
+
 	// EN: Shared private helper functions
 	// DE: Geteilte private Helferfunktionen
-	
+
 	function addEvent (obj, type, fn) {
 		// EN: Feature dection DOM Events / Microsoft
 		// DE: Fähigkeitenweiche DOM Events / Microsoft
@@ -37,13 +37,13 @@ var CopyLink = (function (window, document) {
 			});
 		}
 	}
-	
+
 	// EN: Publish addEvent as a static method
 	// EN: (attach it to the constructor object)
 	// DE: Mache addEvent als statische Methode öffentlich
 	// DE: (hefte die Methode an den Konstruktor, der zurückgegeben wird)
 	CopyLink.addEvent = addEvent;
-	
+
 	function getSelection () {
 		// EN: Feature dection HTML5 / Microsoft
 		// DE: Fähigkeitenweiche HTML5 / Microsoft
@@ -57,13 +57,13 @@ var CopyLink = (function (window, document) {
 			return false;
 		}
 	}
-	
+
 	function getSelectedText (selection) {
 		// EN : Feature detection HTML5 / Microsoft
 		// DE: Fähigkeitenweiche HTML5 / Microsoft
 		return selection.toString ? selection.toString() : selection.text;
 	}
-	
+
 	function removeSpan () {
 		// EN: Is the element attached to the DOM tree?
 		// DE: Ist das Element in den DOM-Baum gehängt?
@@ -76,12 +76,12 @@ var CopyLink = (function (window, document) {
 			parent.removeChild(span);
 		}
 	}
-	
+
 	// EN: Main constructor function
 	// DE: Konstruktorfunktion
 	function CopyLink (options) {
 		var instance = this;
-		
+
 		// EN: Copy members from the options object to the instance
 		// DE: Kopiere Einstellungen aus dem options-Objekt herüber zur Instanz
 		instance.id = options.id || 'copylink';
@@ -90,18 +90,18 @@ var CopyLink = (function (window, document) {
 		instance.handler = options.handler || function () {
 			return '<br>Source: ' + location.href;
 		};
-		
+
 		// EN: Initialisation
 		// DE: Initialisiere
 		instance.create();
 		instance.setupEvents();
 	}
-	
+
 	CopyLink.prototype = {
-		
+
 		create : function () {
 			var instance = this;
-			
+
 			// EN: Create the container for the inserted text if necessary
 			// DE: Erzeuge den Container für den eingefügten Text, sofern noch nicht passiert
 			if (span) {
@@ -111,17 +111,17 @@ var CopyLink = (function (window, document) {
 			span.id = instance.id;
 			span.style.cssText = 'position: absolute; left: -9999px;';
 		},
-		
+
 		setupEvents : function () {
 			var instance = this;
 			addEvent(instance.container, 'copy', function () {
 				instance.insert();
 			});
 		},
-		
+
 		insert : function () {
 			var instance = this;
-			
+
 			// EN: Get a Selection object or a TextRange (IE)
 			// DE: Hole Selection bzw. TextRange (IE)
 			var selection = getSelection();
@@ -130,51 +130,51 @@ var CopyLink = (function (window, document) {
 				// DE: Keine Browser-Unterstützung für die benötigten Features
 				return;
 			}
-			
+
 			// EN: Get the selected text
 			// DE: Hole markierten Text
 			var selectedText = getSelectedText(selection);
-			
+
 			// EN: Abort if the selected text is too short
 			// DE: Breche ab, wenn der markierte Text zu kurz ist
 			if (selectedText.length < instance.minimalSelection) {
 				return;
 			}
-			
+
 			// EN : Feature detection DOM Range / Microsoft
 			// DE: Fähigkeitenweiche DOM Range / Microsoft
 			if (selection.getRangeAt) {
-				
+
 				// EN: W3C DOM Range approach
 				// DE: Lösungsansatz mit W3C DOM Range
-				
+
 				// EN: Get the first Range of the current Selection
 				// DE: Hole Range, die zur Selection gehört
 				var range = selection.getRangeAt(0);
-				
+
 				// EN: Get the start and end nodes of the selection
 				// DE: Hole Start- und Endknoten der Auswahl
 				var startNode = range.startContainer;
 				var endNode = range.endContainer;
-				
+
 				if (!(startNode && endNode && startNode.compareDocumentPosition)) {
 					// EN: Abort if we got bogus values or we can't compare their document position
-					// DE: Breche ab, wenn die Knoten nicht brauchbar sind 
+					// DE: Breche ab, wenn die Knoten nicht brauchbar sind
 					return;
 				}
-				
+
 				// EN: If the start node succeeds the end node in the DOM tree, flip them
 				// DE: Wenn von hinten nach vorne markiert wurde, drehe Start und Ende um
 				if (startNode.compareDocumentPosition(endNode) & 2) {
 					startNode = endNode;
 					endNode = range.startContainer;
 				}
-				
+
 				// EN: Get the start and end offset
 				// DE: Hole Start- und End-Offset
 				var startOffset = range.startOffset;
 				var endOffset = range.endOffset;
-				
+
 				// EN: If the end node is an element, use its last text node as the end offset
 				// DE: Falls der Endknoten ein Element ist, nehme das Ende des letzten Textknoten
 				if (endNode.nodeType == 1) {
@@ -184,42 +184,42 @@ var CopyLink = (function (window, document) {
 					}
 					endOffset = endNode.data.length;
 				}
-				
+
 				// EN: Create a new empty Range
 				// DE: Erzeuge neue, leere Range
 				var newRange = document.createRange();
-				
+
 				// EN: Move the beginning of the new Range to the end of the selection
 				// DE: Verschiebe Anfang der neuen Range an das Ende der Auswahl
 				newRange.setStart(endNode, endOffset);
-				
+
 				// EN: Fill the span containing the source reference
 				// DE: Befülle das span-Element mit der Quellenreferenz
 				span.innerHTML = instance.handler.call(instance);
-				
+
 				// EN: Inject the span element into the new Range
 				// DE: Füge das span-Element in die neue Range ein
 				newRange.insertNode(span);
-				
+
 				// EN: Enlarge the Range forward to enclose the original Range
 				// DE: Erweitere Range nach vorne, um die ursprüngliche einzuschließen
-				
+
 				// EN: Include the span into the selection
 				// DE: Schließe span in die Auswahl ein
 				range.setEndAfter(span);
-				
+
 				// EN: Now select the whole text in the new range.
 				// EN: This text is copied to the clipboard.
 				// DE: Markiere den Text der Range, damit dieser in
 				// DE: die Zwischenablage kopiert wird
 				selection.removeAllRanges();
 				selection.addRange(range);
-				
+
 				window.setTimeout(function () {
 					// EN: Remove the span from the selection
 					// DE: Entferne span wieder aus der Auswahl
 					range.setEndBefore(span);
-					
+
 					// EN: Restore the original range and its text selection
 					// DE: Stelle die ursprüngliche Range und damit die ursprüngliche Textauswahl wieder her
 					if (selection.removeRange) {
@@ -228,63 +228,63 @@ var CopyLink = (function (window, document) {
 						selection.removeAllRanges();
 					}
 					selection.addRange(range);
-					
+
 					// EN: Remove the span from the DOM tree
 					// DE: Entferne span wieder aus dem DOM
 					removeSpan();
 				}, 0);
-				
+
 			} else if (selection.duplicate) {
-				
+
 				// EN: Microsoft TextRange approach
 				// DE: Lösungsansatz mit Microsoft TextRanges
-				
+
 				// EN: Create a copy the the TextRange
 				// DE: Kopiere TextRange
 				var newRange = selection.duplicate();
-				
+
 				// EN: Move the start of the new range to the end of the selection
 				// DE: Verschiebe den Anfang der neuen Range an das Ende der Auswahl
 				newRange.setEndPoint('StartToEnd', selection);
-				
+
 				// EN: Fill the span containing the source reference
 				// DE: Befülle das span-Element mit der Quellenreferenz
 				span.innerHTML = instance.handler.call(instance);
-				
+
 				// EN: Insert the span into the new range
 				// DE: Fülle die neue Range mit dem span
 				newRange.pasteHTML(span.outerHTML);
-				
+
 				// EN: Add the new TextRange to the current selection
 				// DE: Schließe eingefügte TextRange in die Auswahl ein
 				selection.setEndPoint('EndToEnd', newRange);
-				
+
 				// EN: Select the text of the TextRange
 				// EN: This text is copied to the clipboard.
 				// DE: Markiere den Textinhalt der TextRange, damit dieser in
 				// DE: die Zwischenablage kopiert wird
 				selection.select();
-				
+
 				// EN: Since we're using outerHTML to insert the span element,
 				// EN: we have to restore the span reference
 				// DE: Da das Befüllen nicht über das DOM, sondern über serialisierten HTML-Code erfolgt,
 				// DE: stelle die Referenz wieder her
 				span = document.getElementById(id);
-				
+
 				// EN: Remove the span from the DOM tree
 				// DE: Entferne span wieder aus dem DOM
 				window.setTimeout(removeSpan, 0);
-				
+
 			}
-			
+
 			// EN: No browser support available for the required features
 			// DE: Keine Browser-Unterstützung für die benötigten Features
-			
+
 		}
 	};
-	
+
 	// EN: Return the constructor function
 	// DE: Gib den Konstruktor zurück
 	return CopyLink;
-	
+
 })(window, document);
--- selectionmenu.js
+++ selectionmenu.js
@@ -1,7 +1,7 @@
 /*
 SelectionMenu 1.1
 http://github.com/molily/selectionmenu
-by molily (zapperlott@gmail.com, http://molily.de/)
+by molily (molily@mailbox.org, http://molily.de/)
 
 EN: SelectionMenu displays a context menu when the user selects some text on the page
 DE: SelectionMenu blendet ein Kontextmenü beim Markieren von Text ein
@@ -18,14 +18,14 @@ DE: Kopieren, Verteilen und Aendern ohne Einschraenkungen erlaubt
 // DE: Erzeuge einen privaten Scope durch eine anonyme Funktion,
 // DE: speichere den Rückgabwert in einer globalen Variable
 var SelectionMenu = (function (window, document) {
-	
+
 	// EN: The menu element which is inserted when selecting text
 	// DE: Das Menü-Element, welche beim Markieren eingefügt wird
 	var span = null;
-	
+
 	// EN: Shared private helper functions
 	// DE: Geteilte private Helferfunktionen
-	
+
 	function addEvent (obj, type, fn) {
 		// EN: Feature dection DOM Events / Microsoft
 		// DE: Fähigkeitenweiche DOM Events / Microsoft
@@ -37,13 +37,13 @@ var SelectionMenu = (function (window, document) {
 			});
 		}
 	}
-	
+
 	// EN: Publish addEvent as a static method
 	// EN: (attach it to the constructor object)
 	// DE: Mache addEvent als statische Methode öffentlich
 	// DE: (hefte die Methode an den Konstruktor, der zurückgegeben wird)
 	SelectionMenu.addEvent = addEvent;
-	
+
 	function getSelection () {
 		// EN: Feature dection HTML5 / Microsoft
 		// DE: Fähigkeitenweiche HTML5 / Microsoft
@@ -57,20 +57,20 @@ var SelectionMenu = (function (window, document) {
 			return false;
 		}
 	}
-	
+
 	function getSelectedText (selection) {
 		// EN: Feature detection HTML5 / Microsoft
 		// DE: Fähigkeitenweiche HTML5 / Microsoft
 		return selection.toString ? selection.toString() : selection.text;
 	}
 
-	
+
 	function contains (a, b) {
 		// EN: Feature detection DOM Core / Microsoft
 		// DE: Fähigkeitenweiche DOM Core / Microsoft
 		return a.compareDocumentPosition ? !!(a.compareDocumentPosition(b) & 16) : a.contains(b);
 	}
-	
+
 	function mouseOnMenu (e) {
 		// Greife auf das Zielelement des Ereignisses zu
 		// EN: Feature detection DOM Events / Microsoft
@@ -79,12 +79,12 @@ var SelectionMenu = (function (window, document) {
 		// Ist das Zielelement das Menü oder darin enthalten?
 		return target == span || contains(span, target);
 	}
-	
+
 	// EN: Main constructor function
 	// DE: Konstruktorfunktion
 	function SelectionMenu (options) {
 		var instance = this;
-		
+
 		// EN: Copy members from the options object to the instance
 		// DE: Kopiere Einstellungen aus dem options-Objekt herüber zur Instanz
 		instance.id = options.id || 'selection-menu';
@@ -92,70 +92,70 @@ var SelectionMenu = (function (window, document) {
 		instance.minimalSelection = options.minimalSelection || 5;
 		instance.container = options.container;
 		instance.handler = options.handler;
-		
+
 		// EN: Initialisation
 		// DE: Initialisiere
 		instance.create();
 		instance.setupEvents();
 	}
-	
+
 	SelectionMenu.prototype = {
-		
+
 		create : function () {
 			var instance = this;
-			
+
 			// EN: Create the menu container if necessary
 			// DE: Erzeuge den Menü-Container, sofern noch nicht passiert
 			if (span) {
 				return;
 			}
-			
+
 			span = document.createElement('span');
 			span.id = instance.id;
 		},
-		
+
 		setupEvents : function () {
-			
+
 			var instance = this;
 			var container = instance.container;
-			
+
 			// EN: Hide the menu on mouse down
 			// DE: Verstecke beim Mousedown
 			addEvent(container, 'mousedown', function (e) {
 				instance.hide(e);
 			});
-			
+
 			// EN: Insert the menu on mouseup given some text is selected
 			// DE: Füge das Menü beim Mouseup ein, wenn Text ausgewählt wurde
 			addEvent(container, 'mouseup', function (e) {
 				instance.insert(e);
-				
+
 				// EN: After a delay, check if the text was deselected
 				// DE: Prüfe nach einer Verzögerung, ob die Auswahl damit aufgehoben wurde
 				window.setTimeout(function () {
 					instance.hideIfNoSelection();
 				}, 0);
-				
+
 			});
-			
+
 			instance.setupMenuEvents();
 		},
-		
+
 		setupMenuEvents : function () {
 			var instance = this;
-			
+
 			// EN: Register the handler for clicks on the menu
 			// DE: Registiere Handlerfunktion für den Klick auf das Menü
 			addEvent(span, 'click', function (e) {
 				instance.handler.call(instance, e);
 				return false;
 			});
-			
+
 			// EN: Prevent IE to select the text of the menu
 			// DE: Verhindere das Markieren des Menüs im IE
 			span.unselectable = true;
 		},
-		
+
 		hide : function (e) {
 			// EN: Abort if an event object was passed and the click hit the menu itself
 			// Breche ab, wenn Event-Objekt übergeben wurde und der Klick beim Menü passierte
@@ -173,7 +173,7 @@ var SelectionMenu = (function (window, document) {
 				parent.removeChild(span);
 			}
 		},
-		
+
 		hideIfNoSelection : function () {
 			var instance = this;
 			var selection = getSelection();
@@ -185,16 +185,16 @@ var SelectionMenu = (function (window, document) {
 				instance.hide();
 			}
 		},
-		
+
 		insert : function (e) {
 			var instance = this;
-			
+
 			// EN: Abort if the mouse event occured at the menu itself
 			// DE: Breche ab, wenn das Mausereignis beim Menü passierte
 			if (mouseOnMenu(e)) {
 				return;
 			}
-			
+
 			// EN: Get a Selection object or a TextRange (IE)
 			// DE: Hole Selection bzw. TextRange (IE)
 			var selection = getSelection();
@@ -203,52 +203,52 @@ var SelectionMenu = (function (window, document) {
 				// DE: Keine Browser-Unterstützung für die benötigten Features
 				return;
 			}
-			
+
 			// EN: Get the selected text
 			// DE: Hole markierten Text
 			var selectedText = getSelectedText(selection);
 			instance.selectedText = selectedText;
-			
+
 			// EN: Abort if the selected text is too short
 			// DE: Breche ab, wenn der markierte Text zu kurz ist
 			if (selectedText.length < instance.minimalSelection) {
 				instance.hide(e);
 				return;
 			}
-			
+
 			// EN : Feature detection DOM Range / Microsoft
 			// DE: Fähigkeitenweiche DOM Range / Microsoft
 			if (selection.getRangeAt) {
-				
+
 				// EN: W3C DOM Range approach
 				// DE: Lösungsansatz mit W3C DOM Range
-				
+
 				// EN: Get the first Range of the current Selection
 				// DE: Hole Range, die zur Selection gehört
 				var range = selection.getRangeAt(0);
-				
+
 				// EN: Get the start and end nodes of the selection
 				// DE: Hole Start- und Endknoten der Auswahl
 				var startNode = range.startContainer;
 				var endNode = range.endContainer;
-				
+
 				if (!(startNode && endNode && startNode.compareDocumentPosition)) {
 					// EN: Abort if we got bogus values or we can't compare their document position
-					// DE: Breche ab, wenn die Knoten nicht brauchbar sind 
+					// DE: Breche ab, wenn die Knoten nicht brauchbar sind
 					return;
 				}
-				
+
 				// EN: If the start node succeeds the end node in the DOM tree, flip them
 				// DE: Wenn von hinten nach vorne markiert wurde, drehe Start und Ende um
 				if (startNode.compareDocumentPosition(endNode) & 2) {
 					startNode = endNode;
 					endNode = range.startContainer;
 				}
-				
+
 				// EN: Get the end offset
 				// DE: Hole End-Offset
 				var endOffset = range.endOffset;
-				
+
 				// EN: If the end node is an element, use its last text node as the end offset
 				// DE: Falls der Endknoten ein Element ist, nehme das Ende des letzten Textknoten
 				if (endNode.nodeType == 1) {
@@ -258,23 +258,23 @@ var SelectionMenu = (function (window, document) {
 					}
 					endOffset = endNode.data.length;
 				}
-				
+
 				// EN: Create a new empty Range
 				// DE: Erzeuge neue, leere Range
 				var newRange = document.createRange();
-				
+
 				// EN: Move the beginning of the new Range to the end of the selection
 				// DE: Verschiebe Anfang der neuen Range an das Ende der Auswahl
 				newRange.setStart(endNode, endOffset);
-				
+
 				// EN: Fill the menu span
 				// DE: Befülle das Menü-span
 				span.innerHTML = instance.menuHTML;
-				
+
 				// EN: Inject the span element into the new Range
 				// DE: Füge das span-Element in die neue Range ein
 				newRange.insertNode(span);
-				
+
 				// EN: Adjust the selection by removing and adding the range.
 				// EN: This prevents the selection of the menu text.
 				// DE: Korrigiere Auswahl, verhindere das Markieren des Menüs
@@ -284,59 +284,59 @@ var SelectionMenu = (function (window, document) {
 					selection.removeAllRanges();
 				}
 				selection.addRange(range);
-				
+
 			} else if (selection.duplicate) {
-				
+
 				// EN: Microsoft TextRange approach
 				// DE: Lösungsansatz mit Microsoft TextRanges
-				
+
 				// EN: Create a copy the the TextRange
 				// DE: Kopiere TextRange
 				var newRange = selection.duplicate();
-				
+
 				// EN: Move the start of the new range to the end of the selection
 				// DE: Verschiebe den Anfang der neuen Range an das Ende der Auswahl
 				newRange.setEndPoint('StartToEnd', selection);
-				
+
 				// EN: Fill the menu span
 				// DE: Befülle das Menü-span
 				span.innerHTML = instance.menuHTML;
-				
+
 				// EN: Insert the span into the new range
 				// DE: Fülle die neue Range mit dem span
 				newRange.pasteHTML(span.outerHTML);
-				
+
 				// EN: Restore the selection so that the original text is selected
 				// EN: and not the menu
 				// DE: Korrigiere Auswahl und setze sie auf die ursprüngliche Auswahl zurück,
 				// DE: sodass das Menü nicht selektiert ist
 				selection.select();
-				
+
 				// EN: Since we're using outerHTML to insert the span element,
 				// EN: we have to restore the span reference and the event handling
 				// DE: Da das Befüllen nicht über das DOM, sondern über serialisierten HTML-Code erfolgt,
 				// DE: stelle die Referenz und das Event-Handling wieder her
 				span = document.getElementById(id);
 				instance.setupMenuEvents();
-				
+
 			} else {
 				// EN: No browser support available for the required features
 				// DE: Keine Browser-Unterstützung für die benötigten Features
 				return;
 			}
-			
+
 			// EN: Menu positioning
 			// DE: Positioniere Menü
 			instance.position();
 		},
-		
+
 		position : function () {
 			span.style.marginTop = -(span.offsetHeight + 5) + 'px';
 		}
 	};
-	
+
 	// EN: Return the constructor function
 	// DE: Gib den Konstruktor zurück
 	return SelectionMenu;
-	
+
 })(window, document);
